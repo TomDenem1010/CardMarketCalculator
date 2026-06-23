49243d1469e77d8606449bd06cbe8541065b8c77
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2026-06-23 16:58:54 +0200

    NEW: init

---
+++ src/main/java/com/home/cardmarket/CardmarketApplication.java
@@ -0,0 +1,13 @@
+package com.home.cardmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardmarketApplication.class, args);
+	}
+
+}
